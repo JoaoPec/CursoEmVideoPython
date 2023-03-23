d4c266c005f0f732e8629bda924bdbf75119a9b7
João Curcino <125225410+JoaoPec@users.noreply.github.com>
2023-03-23 18:12:59 -0300

    Add files via upload

---
+++ ex037.py
@@ -0,0 +1,18 @@
+n = int(input('Digite um valor)'))
+
+e = int(input('Você quer transformar esse número em: \n 1 -- binario \n 2 -- octal \n 3 -- hexadecimal \n'))
+
+print ('=' * 15)
+print('\n')
+if(e == 1):
+    n = bin(n)
+elif(e == 2):
+    n = oct(n)
+elif(e == 3):
+    n = hex(n)
+else:
+    print('escolha inválida.')
+
+
+print(n[2::])
+    
