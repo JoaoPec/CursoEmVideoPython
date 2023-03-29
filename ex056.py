8bc78ec615bfde9856687aca5aa76fb13cab6faa
João Curcino <125225410+JoaoPec@users.noreply.github.com>
2023-03-29 00:28:19 -0300

    Update ex056.py

--- ex056.py
+++ ex056.py
@@ -1,23 +1,23 @@
-nh = '' 
+nh = ''
 hv = 0
 mv = 0
 ti = 0
 
-for c in range(0,4):
+for c in range(0, 4):
     n = input(str('Digite o seu nome: '))
     i = int(input('Digite a sua idade: '))
     s = input(str('Digite seu sexo (F) ou (M): ')).upper()
-    
+
     if (s == 'M' and i > hv):
         hv = i
         nh = n
     if (s == 'F' and i < 20):
-        mv += 1 
+        mv += 1
     ti += i
 
 m = ti / 4
 
 
-print(f'O homem mais velho é {nh}')
+print(f'O homem mais velho tem {hv} anos e se chama {nh}')
 print(f'Existem {mv} mulheres com menos de 20 anos')
 print(f'A média de idade de todos é: {m}')
